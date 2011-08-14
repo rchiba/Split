fd1efa6f2bf8504410413b6cafe7d4ac91395784
Ryo Chiba <ryochiba@gmail.com>
2011-08-14 12:12:42 -0700

    Scrolling works.

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -12,15 +12,17 @@ window.onscroll=function(){
             });
     }
 
-    // triggered by the background.html if message needs to be sent to client DOM
-    chrome.extension.onRequest.addListener(
-        function(request,sender,sendResponse){
-            if (request.action === 'scrollTo'){
-               // mootools scroll
-               var myFx = new Fx.Scroll($(window)).start(request.data.pageXOffset,request.data.pageYOffset); 
-            }
-        }
-    );
-
 }
 
+// triggered by the background.html if message needs to be sent to client DOM
+chrome.extension.onRequest.addListener(
+    
+    (function(request,sender,sendResponse){
+        if (request.action === 'scrollTo'){
+           console.log('scrollTo heard with'+request.data.pageXOffset+' '+request.data.pageYOffset);
+           $('html, body').animate({
+                scrollTop:request.data.pageYOffset,
+                scrollLeft:request.data.pageXOffset
+                }, 1000);
+        }
+    }).bind(this));
